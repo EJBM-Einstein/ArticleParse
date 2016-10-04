c1ae1393f8b3717c4dd174a4cf7134c70805ab9d
mes32 <stockman.mike@gmail.com>
2016-10-03 22:16:09 -0400

    feature: Print purpose and usage if called without arguments

--- src/main/java/parse/ArticleParse.java
+++ src/main/java/parse/ArticleParse.java
@@ -8,8 +8,25 @@ public class ArticleParse {
 
     public static void main(String[] args) {
 
-        System.out.println("Running Article Parse");
+    	if (args.length == 0) {
+    		printUsageInterface();
+    		System.exit(1);
+    	}
 
+        System.out.println("Running Article Parse... (prints PDFs encountered)");
+
+    }
+
+    private static void printUsageInterface() {
+    	String message = "";
+    	message += "+++ ArticleParse +++\n";
+    	message += "\n";
+    	message += "    PURPOSE: Given a list of locations (files or directories) reads each PDF document encountered.\n";
+    	message += "    The content of the PDF is semantically parsed. For example the authors of the article can be selected and saved.\n";
+    	message += "\n";
+    	message += "    USAGE: ArticleParse <location_1> [ .. <location_n> ]\n";
+
+    	System.err.print(message);
     }
 
 }
